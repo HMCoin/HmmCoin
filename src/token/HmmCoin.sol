c380ec9ba3d133f7b27a48014ca185c0197e68f1
pingwindyktator <ja2222ja@gmail.com>
2021-03-11 17:25:06 +0100

    Add initial source code

---
+++ src/token/HmmCoin.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.0;
+
+import "./ERC20/ERC20Capped.sol";
+import "../access/AccessControl.sol";
+import "./ERC20/ERC20Burnable.sol";
+
+contract HmmCoin is Context, AccessControl, ERC20Capped, ERC20Burnable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    string public constant name = "HmmCoin";
+    string public constant symbol = "hmm";
+    uint public constant decimals = 18;
+
+    // @param _initialSupply Initial supply of the contract that will be minted into owner's account
+    // @param _maxSupply Maximum possible tokens cap
+    function HmmCoin(uint256 _initialSupply, uint256 _maxSupply) ERC20(name, symbol) ERC20Capped(_maxSupply) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+
+        _totalSupply = _initialSupply;
+        balances[owner] = _initialSupply;
+    }
+
+    /**
+    * @dev Creates `amount` new tokens for `to`.
+     *
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC20PresetMinterPauser: must have minter role to mint");
+        _mint(to, amount);
+    }
+}
